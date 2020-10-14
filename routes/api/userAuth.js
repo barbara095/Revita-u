39a596bd4604a792b59eb5e72c50501914bfec95
Barbs <barbarap095@gmail.com>
2020-10-14 21:18:17 +1100

    deploy app

--- routes/api/userAuth.js
+++ routes/api/userAuth.js
@@ -1,4 +1,6 @@
 const User = require("../../models/user");
+const router = require('express').Router();
+const passport = require('../../config/passport');
 
 module.exports = (app) => {
 // For sign up
