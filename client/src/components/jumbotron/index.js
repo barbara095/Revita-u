66aa1895a242eeb04e5fa8525c01ecc522946331
Barbs <barbarap095@gmail.com>
2020-10-14 22:01:47 +1100

    deploy app

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -1,14 +1,10 @@
 import React from "react";
-import SearchForm from "../searchForm";
-import FormBtn from "../Button";
 import "./style.css";
 
 function Jumbotron() {
   return (
     <div className="jumbotron">
       <h1 className="jumbotron-header">Search for recipes by ingredient...</h1>
-      {/* <SearchForm />
-      <FormBtn /> */}
     </div>
   );
 }
--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import RecipeContainer from "../components/recipeContainer/recipeContainer";
-import Jumbotron from "..components/Jumbotron";
+import Jumbotron from "../components/Jumbotron";
 import Wrapper from "../components/Wrapper";
 import { Container } from "../components/Grid";
 
