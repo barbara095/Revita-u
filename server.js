88c7a1eb1cb1becd96ed445c044f0f79dda9a9b2
Barbs <barbarap095@gmail.com>
2020-10-16 11:45:00 +1100

    deploy app

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI ||
