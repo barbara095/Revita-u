d7158a417199199a4fed35d8aa5d8a8adc4590a3
Barbs <barbarap095@gmail.com>
2020-10-16 11:56:03 +1100

    deploy app

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ app.use(passport.session());
 
 // Accessing public directory
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 mongoose.connect(process.env.MONGODB_URI ||
